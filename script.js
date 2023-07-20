bf76b9d15da40a4c06ec5cfd7a28c9340a983fe4
Samael Melo <samaelmelo.dev@gmail.com>
2023-07-20 15:12:41 -0400

    nao permitir que um valor se repita

--- script.js
+++ script.js
@@ -1,50 +1,57 @@
-
-const listaDeChamadas = [
-  "Sombreia",
-  "Istrondou na barra",
-  "Minguarana",
-  "Caiano",
-  "Chamada da união",
-  "Hoasca",
-  "Cajueiro",
-  "Todo cipó",
-  "Princesa Mariana",
-  "Lago de Burracheira",
-  "Amor de Mãe",
-  "Nuvem clara e pura",
-  "Estrela do norte",
-  "Correr para onde tem sombra",
-  "14 anjos divinos",
-  "Grande foco",
-  "Castelo da ciência",
-  "Chamada das cores do uniforme",
-  "Quando eu quero dou um nó",
-  "Tiuaco",
-  "Vitória",
-  "Chamada do silêncio",
-  "Rei rabino",
-  "Expansão",
-  "Guarnição divina",
-  "Socorro divino",
-  "Estrela do oriente"
-]
+let listaDeChamadas = [
+  'Sombreia',
+  'Istrondou na barra',
+  'Minguarana',
+  'Caiano',
+  'Chamada da união',
+  'Hoasca',
+  'Cajueiro',
+  'Todo cipó',
+  'Princesa Mariana',
+  'Lago de Burracheira',
+  'Amor de Mãe',
+  'Nuvem clara e pura',
+  'Estrela do norte',
+  'Correr para onde tem sombra',
+  '14 anjos divinos',
+  'Grande foco',
+  'Castelo da ciência',
+  'Chamada das cores do uniforme',
+  'Quando eu quero dou um nó',
+  'Tiuaco',
+  'Vitória',
+  'Chamada do silêncio',
+  'Rei rabino',
+  'Expansão',
+  'Guarnição divina',
+  'Socorro divino',
+  'Estrela do oriente',
+];
 
 const escolherUmaChamada = () => {
-  
-let escolhaAleatoriaPeloIndex = Math.ceil(Math.random() * listaDeChamadas.length)
+  let escolhaAleatoriaPeloIndex = Math.ceil(
+    Math.random() * listaDeChamadas.length,
+  );
 
-let chamadaEscolhida = listaDeChamadas[escolhaAleatoriaPeloIndex]?.toLowerCase()
+  let chamadaEscolhida =
+    listaDeChamadas[escolhaAleatoriaPeloIndex]?.toLowerCase();
+
+  listaDeChamadas = listaDeChamadas.filter(
+    chamada => chamada !== listaDeChamadas[escolhaAleatoriaPeloIndex],
+  );
 
   return `
-Treine a ${ chamadaEscolhida.includes('chamada') ? chamadaEscolhida : `chamada ${chamadaEscolhida}` }`
-
-}
-
+Treine a ${
+    chamadaEscolhida.includes('chamada')
+      ? chamadaEscolhida
+      : `chamada ${chamadaEscolhida}`
+  }`;
+};
 
 random.addEventListener('click', () => {
-  chamadaSorteada.innerText = '... carregando'
+  chamadaSorteada.innerText = '... carregando';
 
- setTimeout(() => {
-  chamadaSorteada.innerText = escolherUmaChamada()
- },1000)
-})
+  setTimeout(() => {
+    chamadaSorteada.innerText = escolherUmaChamada();
+  }, 1000);
+});
